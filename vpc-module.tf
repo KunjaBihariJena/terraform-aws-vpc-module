6847d5c530a3d2a62e86a56aae912ed9743f026f
EC2 Default User <ec2-user@ip-172-31-32-91.ec2.internal>
2023-05-03 08:47:57 +0000

    vpc-simple module

---
+++ vpc-module.tf
@@ -0,0 +1,35 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "vpc-main"
+  }
+}
+
+resource "aws_subnet" "us-east-1a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "us-east-1a"
+  }
+}
+
+resource "aws_subnet" "us-east-1b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "us-east-1b"
+  }
+}
+
+resource "aws_subnet" "us-east-1c" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1c"
+  tags = {
+    Name = "us-east-1c"
+  }
+}
